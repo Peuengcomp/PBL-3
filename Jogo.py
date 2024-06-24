8418ada2e29ab3c76ec402569f643f0e4d8af967
Pedro Fernandes <165844447+Peuengcomp@users.noreply.github.com>
2024-06-24 16:10:18 -0300

    Update Jogo.py

--- Jogo.py
+++ Jogo.py
@@ -72,95 +72,134 @@ def imprimir_tela(matriz):
             print(f"|{elemento}|", end='\t')
         print("\n")
 
-def sorteio(jogador):
-    print(f"Jogador {jogador}:\n")
-    nome = input(f"Digite o nome do jogador {jogador}: ")
-    objetivo = rd.randint(1,4)
-    if objetivo == 1:
-        print("Seu objetivo é formar uma sequência numérica ascendente\n")
-    elif objetivo == 2:
-        print("Seu objetivo é formar uma sequência numérica descendente\n")
-    elif objetivo == 3:
-        print("Seu objetivo é formar uma sequência numérica par\n")
-    else:
-        print("Seu objetivo é formar uma sequência numérica ímpar\n")
-    aux = int(input(f"Digite {jogador} para prosseguir: "))
-    while aux != jogador:
-        aux = int(input(f"Digite {jogador} para prosseguir: "))
-    return (objetivo,nome,jogador)
+##################################################################################################################################
 
-def jogada(dicionario, jogador):
-    global azul, vermelho, reset
+def sorteio(jogador):
+    global verde, azul, vermelho, reset
     if jogador == 1:
         cor = azul
     else:
         cor = vermelho
-    print(cor + f"Faça sua jogada, jogador {jogador}" + reset)
+    
+    print(verde + f"Jogador {jogador}:\n" + reset)
+    print(verde + f"Digite o nome do jogador {jogador}: " + reset, end='')
+    nome = input()
+    print("\n")
+    objetivo = rd.randint(1,4)
+    if objetivo == 1:
+        print(cor + "Seu objetivo é formar uma sequência numérica ascendente\n" + reset)
+    elif objetivo == 2:
+        print(cor + "Seu objetivo é formar uma sequência numérica descendente\n" + reset)
+    elif objetivo == 3:
+        print(cor + "Seu objetivo é formar uma sequência numérica par\n" + reset)
+    else:
+        print(cor + "Seu objetivo é formar uma sequência numérica ímpar\n" + reset)
+    
+    print(verde + f"Digite {jogador} para prosseguir: " + reset, end='')
+    aux = int(input())
+    while aux != jogador:
+        aux = int(input(f"Digite {jogador} para prosseguir: "))
+    return (objetivo,nome,jogador)
+
+def gerar_numeros_possiveis(tamanho):
+    numeros = []
+    for n in range(1, tamanho**2 + 1):
+        numeros.append(n)
+    return numeros
+
+def jogada(dicionario, jogador, numeros, nome1, nome2):
+    global azul, vermelho, reset
+    if jogador == 1:
+        cor = azul
+        nome = nome1
+    else:
+        cor = vermelho
+        nome = nome2
+    print(cor + f"Faça sua jogada, jogador {nome}" + reset)
     linha = int(input("Digite a linha\n"))
     coluna = int(input("Digite a coluna\n"))
     if dicionario.get((linha,coluna), False) == "x":
         jogada = int(input("Digite seu número\n"))
-        dicionario[(linha,coluna)] = str(cor + str(jogada) + reset)
-        return dicionario, jogador, jogada, linha, coluna
+        if jogada in numeros:
+            dicionario[(linha,coluna)] = str(cor + str(jogada) + reset)
+            return dicionario, jogada, linha, coluna
+        else:
+            return None
     else:
         return None
     
-def razao_numerica(objetivo):
-    if objetivo == 1:
-        return 1
-    elif objetivo == 2:
-        return -1
-    else:
-        return 2
+##################################################################################################################################
 
 def verificar_lista(lista, objetivo):
+    if objetivo == 1:
+        razao = 1
+    elif objetivo == 2:
+        razao = -1
+    else:
+        razao = 2
+    
     for k in range(len(lista) - 1):
-        if lista[k+1] - lista[k] != razao_numerica(objetivo):
+        if lista[k+1] - lista[k] != razao:
             resultado = False
             break
         else:
             resultado = True
     if resultado == True:
         return resultado
-    
 
-def verificar_linha_coluna(dicionario, tamanho, objetivo, nome, jogador):
+def verificar_linha_coluna_diagonal(dicionario, tamanho, objetivo, nome, jogador):
     for i in range(1, tamanho + 1):
         linha = []
         coluna = []
+        principal = []
+        secundaria = []
+
         for j in range(1, tamanho + 1):
             linha.append(dicionario[(i,j)])
             coluna.append(dicionario[(j,i)])
+            principal.append(dicionario[(j,j)])
+            secundaria.append(dicionario[(tamanho + 1 - j,j)])
+
         if objetivo == 1:
             resultado_linha = verificar_lista(linha, objetivo)
             resultado_coluna = verificar_lista(coluna, objetivo)
-            if resultado_linha == True or resultado_coluna == True:
+            resultado_principal = verificar_lista(principal, objetivo)
+            resultado_secundaria = verificar_lista(secundaria, objetivo)
+            if resultado_linha == True or resultado_coluna == True or resultado_principal == True or resultado_secundaria == True:
                 return (True,nome,jogador)
             else:
                 return (False,nome,jogador)
         elif objetivo == 2:
             resultado_linha = verificar_lista(linha, objetivo)
             resultado_coluna = verificar_lista(coluna, objetivo)
-            if resultado_linha == True or resultado_coluna == True:
+            resultado_principal = verificar_lista(principal, objetivo)
+            resultado_secundaria = verificar_lista(secundaria, objetivo)
+            if resultado_linha == True or resultado_coluna == True or resultado_principal == True or resultado_secundaria == True:
                 return (True,nome,jogador)
             else:
                 return (False,nome,jogador)
         elif objetivo == 3:
-            if linha[0] % 2 == 0 or coluna[0] % 2 == 0:
+            if linha[0] % 2 == 0 or coluna[0] % 2 == 0 or principal[0] % 2 == 0 or secundaria[0] % 2 == 0:
                 resultado_linha = verificar_lista(linha, objetivo)
                 resultado_coluna = verificar_lista(coluna, objetivo)
-                if resultado_linha == True or resultado_coluna == True:
+                resultado_principal = verificar_lista(principal, objetivo)
+                resultado_secundaria = verificar_lista(secundaria, objetivo)
+                if resultado_linha == True or resultado_coluna == True or resultado_principal == True or resultado_secundaria == True:
                     return (True,nome,jogador)
                 else:
                     return (False,nome,jogador)
         else:
-            if linha[0] % 2 != 0 or coluna[0] % 2 != 0:
+            if linha[0] % 2 != 0 or coluna[0] % 2 != 0 or principal[0] % 2 != 0 or secundaria[0] % 2 != 0:
                 resultado_linha = verificar_lista(linha, objetivo)
                 resultado_coluna = verificar_lista(coluna, objetivo)
-                if resultado_linha == True or resultado_coluna == True:
+                resultado_principal = verificar_lista(principal, objetivo)
+                resultado_secundaria = verificar_lista(secundaria, objetivo)
+                if resultado_linha == True or resultado_coluna == True or resultado_principal == True or resultado_secundaria == True:
                     return (True,nome,jogador)
                 else:
                     return (False,nome,jogador)
+
+##################################################################################################################################
 
 start = True
 while start:
@@ -177,26 +216,36 @@ while start:
         jogador = rd.randint(1,2)
         matriz_visual, matriz_de_jogadas, tamanho = nivel(opcao)
         imprimir_tela(formatar_tela(tamanho, matriz_visual))
+        numeros = gerar_numeros_possiveis(tamanho)
         while rodar:
-            valor = jogada(matriz_visual, jogador)
+            valor = jogada(matriz_visual, jogador, numeros, tupla_1[1], tupla_2[1])
             resultado = False
             if valor == None:
-                print("Não é possível fazer essa jogada\n")         
+                os.system('clear')
+                imprimir_tela(formatar_tela(tamanho, matriz_visual)) 
+                print("Não é possível fazer essa jogada\n")       
             else:
-                matriz_visual, jogador, lance, linha, coluna = valor
+                matriz_visual, lance, linha, coluna = valor
                 matriz_de_jogadas[(linha,coluna)] = lance
+                numeros.remove(lance)
 
-                resultado_1 = verificar_linha_coluna(matriz_de_jogadas, tamanho, tupla_1[0], tupla_1[1], tupla_1[2])
+                resultado_1 = verificar_linha_coluna_diagonal(matriz_de_jogadas, tamanho, tupla_1[0], tupla_1[1], tupla_1[2])
                 valor_1 = resultado_1[0]
-                resultado_2 = verificar_linha_coluna(matriz_de_jogadas, tamanho, tupla_2[0], tupla_2[1], tupla_2[2])
+                resultado_2 = verificar_linha_coluna_diagonal(matriz_de_jogadas, tamanho, tupla_1[0], tupla_2[1], tupla_2[2])
                 valor_2 = resultado_2[0]
                 if valor_1 == True or valor_2 == True:
                     if valor_1 == True and valor_2 == True:
                         resultado = True
+                        if jogador == 1:
+                            nome = tupla_1[1]
+                        else:
+                            nome = tupla_2[1]
                     elif valor_1 == True:
+                        nome = tupla_1[1]
                         jogador = tupla_1[2]
                         resultado = True
                     else:
+                        nome = tupla_2[1]
                         jogador = tupla_2[2]
                         resultado = True
                 else:
@@ -205,7 +254,7 @@ while start:
                 if resultado == True:
                     os.system('clear')
                     imprimir_tela(formatar_tela(tamanho, matriz_visual))
-                    print(f"O vencedor foi o jogador {jogador}\n")
+                    print(f"O vencedor foi o jogador {jogador}, {nome}\n")
                     rodar = False
                 
                 else:
@@ -213,8 +262,11 @@ while start:
                     imprimir_tela(formatar_tela(tamanho, matriz_visual))
                     if jogador == 1:
                         jogador = 2
+                        nome = tupla_2[1]
                     else:
                         jogador = 1
+                        nome = tupla_1[1]
+                
     elif escolha == 2:
         print("Continuar jogo")
     
