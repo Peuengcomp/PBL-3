03df17a2d5887a79c866606ad3ff6e79d21544bc
Pedro Fernandes <165844447+Peuengcomp@users.noreply.github.com>
2024-07-01 22:49:29 -0300

    Update Jogo.py

--- Jogo.py
+++ Jogo.py
@@ -1,3 +1,16 @@
+'''
+Autor: Pedro Lucas Fernandes de Souza
+Componente Curricular: Algoritmos I
+Concluido em: 01/07/2024
+
+Declaro que este código foi elaborado por mim de forma individual e não contém nenhum trecho de código de outro colega ou de outro autor, 
+tais como provindos de livros e apostilas, e páginas ou documentos eletrônicos da Internet. 
+Qualquer trecho de código de outra autoria que não a minha está destacado com uma citação para o autor e a fonte do código, 
+tais como provindos de livros e apostilas, e páginas ou documentos eletrônicos da Internet. 
+Qualquer trecho de código de outra autoria que não a minha está destacado com uma citação para o autor e a fonte do código, 
+e estou ciente que estes trechos não serão considerados para fins de avaliação.
+'''
+
 """
 Este bloco de funções é responsável por declarar as bibliotecas necessárias ao jogo, além
 de declarar as cores em tabela ANSI para tornar a experiência mais interativa.
